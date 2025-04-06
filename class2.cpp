63f8bbe192d8defcdbb95323011b2b85898521d2
dendi cay <munoz.raiden@ue.edu.ph>
2025-04-06 18:26:59 +0000

    basic constructor oop

---
+++ class2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    int width, height;
+  public:
+    Rectangle (int,int);
+    int area () {return (width*height);}
+};
+
+Rectangle::Rectangle (int a, int b) {
+  width = a;
+  height = b;
+}
+
+int main () {
+  Rectangle rectA (3,4);
+  Rectangle rectB (5,6);
+  
+  cout << "Area of Rectangle A: " << rectA.area() << endl;
+  cout << "Area of Rectangle B: " << rectB.area() << endl;
+  cout << “RAIDEN MUNOZ – 1CPE – 2B” << endl;
+  system("pause");
+}
