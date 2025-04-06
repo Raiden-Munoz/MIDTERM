520f5f2562a3ea194732fd9a98a11078c8332488
dendi cay <munoz.raiden@ue.edu.ph>
2025-04-06 18:27:42 +0000

    default constructor oop

---
+++ class3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    int width, height;
+  public:
+    Rectangle ();
+    Rectangle (int,int);
+    int area () {
+        return (width*height);
+    }
+};
+
+Rectangle::Rectangle () {
+  width = 5;
+  height = 5;
+}
+
+Rectangle::Rectangle (int a, int b) {
+  width = a;
+  height = b;
+}
+
+int main () {
+  Rectangle rectA (3,4);
+  Rectangle rectB;
+  cout << "Area of Rectangle A: " << rectA.area() << endl;
+  cout << "Area of Rectangle B: " << rectB.area() << endl;
+  cout << "RAIDEN MUNOZ - 1CPE - 2B" << endl;
+  system("pause");
+}
