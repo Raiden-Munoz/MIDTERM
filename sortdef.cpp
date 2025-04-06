cb327ee1e7eaa51c1d85d5cddfae866c89e7ed29
dendi cay <munoz.raiden@ue.edu.ph>
2025-04-06 17:56:48 +0000

    sorting definition

---
+++ sortdef.cpp
@@ -0,0 +1,22 @@
+sorting algorithms;
+an algorith made up of a series of insturctions that takes
+an array as input, and outputs a sorted array 
+
+merge sort:
+a sorting technique based on divide and conquer technique
+with worst case time complexity of O(nlogn), it is one of the most respected
+algorithms
+
+it first divides the array into equel halves and then combines them in a sorted
+manner
+
+bubble sort:
+simpliest sorting algorithm that works by repeatedly swapping the adjacent elements
+if they are in the wrong order
+
+selection sort:
+sorts an array by repeatedly finding the minimum element from unsorted part and putting it at the beginning
+
+insertion sort:
+a simple sorting algorithm that works the way we sort playing cards
+in our hands 
