ffc38f14a8af0849f9d1c1fe91b22470d8da8223
dendi cay <munoz.raiden@ue.edu.ph>
2025-04-06 18:15:54 +0000

    searching definition

---
+++ searchdef.cpp
@@ -0,0 +1,27 @@
+searching algorith:
+algorithm made up of a series of instructions that retrieves information
+stored within some data structure, or calculated 
+in the search space of a problem domain
+
+linear search:
+for finding a target value within a list of values.
+it sequentially checks each element of the list 
+for the target value until a match is found or
+until all the elements have been searched.
+
+binary search:
+the most popular. it is efficient and also one of the 
+most commonly used techniques that is used to solve 
+problems. binary search use sorted array by repeatedly
+dividing the search interval in half
+
+jump search:
+for sorted arrays. the basic idea is to check fewer
+elements (than linear search) by jumping ahead by 
+fixed steps or skipping some elements in place of
+searching all elements
+
+interpolation search:
+an imporvement over binary search for instances. on the other
+hand interpolation seach may go to different locations
+according the value of key being searched.
