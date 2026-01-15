60317c1ca2e20c39419a7534ab62e03b88bc3310
dendi cay <munoz.raiden@ue.edu.ph>
2026-01-15 23:21:54 +0000

    FORK

--- 1.cpp
+++ 1.cpp
@@ -86,6 +86,7 @@ int main()
     return 0;
 }
 */
+/*
 #include <iostream>
 using namespace std;
 
@@ -145,4 +146,28 @@ int main()
     cout << "\nAverage Turnaround Time = " << tatavg / n;
     return 0;
 }
-
+*/
+#include <stdio.h>
+#inlclude <stdlib.h>
+#include <unistd.h>
+#iclude <sys/wait.h>
+void main(int argc, char *arg[])
+{
+int pid; pid = fork();
+if (pid < 0)
+{
+printf(“fork failed”)’
+exit (1);
+}
+else if (pid == 0)
+{
+execlp(“whoami”, “1s”, NULL);
+exit (0);
+}
+else
+{
+printf(“\n Process ID is: %d\n”, getpid());
+wait (NULL);
+exit (0);
+}
+}
